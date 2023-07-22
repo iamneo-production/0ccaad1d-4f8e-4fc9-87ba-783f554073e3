2c339bff56978dd47e40cc418b669ca380cacbf1
Lingesh002 <radhasathiya35@gmail.com>
2023-07-22 18:23:04 +0000

    update

--- dotnetapp/Startup.cs
+++ dotnetapp/Startup.cs
@@ -30,7 +30,7 @@ namespace dotnetapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connectionString = Configuration.GetConnectionString("myconnstring");
+           string connectionString = Configuration.GetConnectionString("myconnstring");
             services.AddDbContext<DataDbContext>(opt => opt.UseSqlServer(connectionString));
             services.AddScoped<IEmailService, EmailService>();
             services.AddCors(options =>
