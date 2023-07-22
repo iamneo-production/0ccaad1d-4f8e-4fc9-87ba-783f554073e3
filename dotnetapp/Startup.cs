6bde606f4d450426152e2f3065b0375021600a5d
Brindha910 <vbrindha910@gmail.com>
2023-07-22 11:20:55 +0000

    jj

--- dotnetapp/Startup.cs
+++ dotnetapp/Startup.cs
@@ -10,8 +10,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
-namespace WebApp
+namespace dotnetapp
 {
     public class Startup
     {
@@ -22,11 +25,29 @@ namespace WebApp
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //string connectionString = Configuration.GetConnectionString("myconnstring");
+           // services.AddDbContext<ProductDBContext>(opt => opt.UseSqlServer(connectionString));
+           // services.AddScoped<IProductService, ProductService>();
+            services.AddCors();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnetapp", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dotnetapp v1"));
             }
 
             app.UseHttpsRedirection();
