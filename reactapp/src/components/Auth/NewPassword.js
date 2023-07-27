8be2ae8005a910bdd8e2b5fae03a80ec485e0afa
DevarapuChaitanyaPriya <dchaitanya501@gmail.com>
2023-07-27 18:02:30 +0000

    hello

--- reactapp/src/components/Auth/NewPassword.js
+++ reactapp/src/components/Auth/NewPassword.js
@@ -17,7 +17,7 @@ const NewPassword = () => {
     e.preventDefault();
     if (password === confirmPassword) {
       axios
-        .put("https://8080-fecfcfddebecabebafcdcbccefeddcbcbaffb.project.examly.io/resetpassword", {
+        .put("https://8080-beacfbfacaabbdffbebafcdcbccefeddcbcbaffb.project.examly.io/resetpassword", {
           email: email,
           password: password,
         })
