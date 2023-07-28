299d17914ef9fbf78815f18d34d08bfcd9ea1b06
Lingesh002 <radhasathiya35@gmail.com>
2023-07-28 06:51:32 +0000

    okay

--- reactapp/src/components/Auth/Login.js
+++ reactapp/src/components/Auth/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
     const email = e.target.elements.email.value;
     const password = e.target.elements.password.value;
 console.log(email, password)
-    axios.post('https://8080-fecfcfddebecabebafcdcbccefeddcbcbaffb.project.examly.io/login', { "email" : email
+    axios.post('https://8080-bdfdeaacbaffbebafcdcbccefeddcbcbaffb.project.examly.io/login', { "email" : email
     , "password": password })
 
       .then(response => {
