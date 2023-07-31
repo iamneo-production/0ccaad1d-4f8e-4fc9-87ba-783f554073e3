83097fd23a472cd1899f86571de87bc740ecb269
raghulmkp <raghulmkp@gmail.com>
2023-07-31 05:39:28 +0000

    property file

---
+++ reactapp/src/components/Admin/AdminGifts/EditGifts.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import AdminNavigation from '../AdminNavigation/AdminNavigation'
+import EditGift from './EditGift'
+import "./EditGifts.css"
+const EditGifts = () => {
+  const id = [
+    'editGiftName','editGiftPrice','editGiftImageUrl','editGiftQuantity', 'editGiftDetails','updateGiftButton','UPDATE'
+  ]
+  return (
+    <>
+    <AdminNavigation/>
+    <div className='editGifts-wrapper'>
+      <EditGift id = {id}/>
+    </div>
+    </>
+  )
+}
+
+export default EditGifts
