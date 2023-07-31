2c06081e0272809c06befddf9156e0166f0e86fc
raghulmkp <raghulmkp@gmail.com>
2023-07-31 04:26:14 +0000

    commit

--- nunit/run.sh
+++ nunit/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp -r /home/coder/project/workspace/nunit/test/WebAppTest /home/coder/project/workspace/dotnetapp;
-cd /home/coder/project/workspace/dotnetapp;
+cp -r /home/coder/project/workspace/nunit/test/WebAppTest /home/coder/project/workspace/WebApp;
+cd /home/coder/project/workspace/WebApp;
 dotnet test
